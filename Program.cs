a8165492ee8a7b0c329a9c1b007c05338371cef7
GaussianEntrail <53263900+GaussianEntrail@users.noreply.github.com>
2022-09-05 11:42:35 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using CipherThing;
 //4 - keyword for enciphering text
 //5 - path of custom polybius square
 string[] getArgs = Environment.GetCommandLineArgs();
-string inputFilePath = getArgs[1], outputFilePath = getArgs[2], gridAlterKeyword = "", cipherKeyword = "", pathCustomSquare = "";
+string gridAlterKeyword = "", cipherKeyword = "", pathCustomSquare = "";
 bool doGridAltering = false, useKeyword = false, useCustomSquare = false;
 
 if (getArgs.Length > 3)
@@ -39,38 +39,22 @@ if (getArgs.Length > 5)
 
 //Take the file at getArgs[1] and encipher it, putting the output in getArgs[2]
 //Use the optional arguments in getArgs[3] and getArgs[4] to shuffle the polybius square or as a keyword
-string[] I = System.IO.File.ReadAllLines(inputFilePath);
-string[] O = new string[I.Length];
 
 if (doGridAltering && !String.IsNullOrEmpty(gridAlterKeyword)) 
 { 
     Ciphers.PolybiusSquareShift(gridAlterKeyword, useCustomSquare); 
 }
-
-for (int k = 0; k < I.Length; k++)
+try
 {
-    O[k] = CipherThing.Ciphers.EncodePolybiusToString(I[k], cipherKeyword, useKeyword);
-}
-
-System.IO.File.WriteAllLines(outputFilePath, O);
-
-#region old demo code
-//string plaintext = "Meet Me At The Fountain";
-//string[] testkeys = { "LEMON", "ORANGE", "NAZARETH2" };
-//string ciphertext, ciphertext2;
-
-//Console.WriteLine("Polybius square cipher test!\nBefore Shuffling:\n");
-
-//Console.WriteLine(Ciphers.Polybius.ToMatrixString());
-
-//Console.WriteLine("Use keyword {0} to alter the grid", testkeys[2] );
-
-//Ciphers.PolybiusSquareShift(testkeys[2]);
-
-//Console.WriteLine(Ciphers.Polybius.ToMatrixString());
-
-//ciphertext = CipherThing.Ciphers.EncodePolybiusToString(plaintext, " ", false);
-//ciphertext2 = CipherThing.Ciphers.EncodePolybiusToString(plaintext, testkeys[0], true);
-
-//Console.WriteLine("Polybius Square cipher test, using this shuffled matrix\n\nPLAINTEXT:\n\t{0}\nCIPHERTEXT:\n\t{1}\nCIPHERTEXT (USING KEYWORD {2}):\n\t{3}",plaintext,ciphertext,testkeys[0],ciphertext2);
-#endregion
+    using (StreamReader I = new(getArgs[1]))
+    {
+        using (StreamWriter O = new(getArgs[2]))
+        {
+            string? line;
+            while ((line = I.ReadLine()) != null)
+            {
+                O.WriteLine(Ciphers.EncodePolybiusToString(line, cipherKeyword, useKeyword));
+            }
+        }
+    }
+} catch (Exception ex) { Console.WriteLine(ex.Message); }
